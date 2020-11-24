c867396a3f413fdeea53cf9d323100fb0c927467
Jaewoong Lee <230@fumire.moe>
2020-11-24 13:57:04 +0900

    Add step01

---
+++ jwlee230/Program/Python/step01.py
@@ -0,0 +1,24 @@
+"""
+step01: make manifest file
+"""
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input", help="Input FASTQ.gz files", type=str, nargs="+")
+
+    args = parser.parse_args()
+
+    args.input.sort()
+
+    f1 = sorted(list(filter(lambda x: x.endswith("1.fastq.gz"), args.input)))
+    f2 = sorted(list(filter(lambda x: x.endswith("2.fastq.gz"), args.input)))
+
+    print("sample-id\tforward-absolute-filepath\treverse-absolute-filepath")
+    for fa, fb in zip(f1, f2):
+        name = fa[:-11]
+        if fb.startswith(name):
+            print(name[name.rfind("/") + 1:], fa, fb, sep="\t")
+        else:
+            raise ValueError(fa, fb)
