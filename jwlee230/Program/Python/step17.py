21748e303aecd4cab77ed64fc657d6a38a2e6f9a
Jaewoong Lee <230@fumire.moe>
2020-11-27 12:43:18 +0900

    Add stage columns

--- jwlee230/Program/Python/step17.py
+++ jwlee230/Program/Python/step17.py
@@ -19,12 +19,13 @@ if __name__ == "__main__":
         raise ValueError("ANCOM file must end with .TSV!!")
 
     data = step00.read_pickle(args.input)
+    print(data)
 
     ancom_data = pandas.read_csv(args.ancom, sep="\t", names=["Bacteria", "W", "Reject"], header=0)
     ancom_data["Bacteria"] = list(map(lambda x: step00.consistency_taxonomy(x), ancom_data["Bacteria"]))
     print(ancom_data)
 
-    data = data[sorted(list(map(lambda x: x[0], list(filter(lambda x: x[1], zip(ancom_data["Bacteria"], ancom_data["Reject"]))))))]
+    data = data[sorted(list(map(lambda x: x[0], list(filter(lambda x: x[1], zip(ancom_data["Bacteria"], ancom_data["Reject"])))))) + ["ShortStage" + "LongStage"]]
 
     print(data)
     step00.make_pickle(args.output, data)
