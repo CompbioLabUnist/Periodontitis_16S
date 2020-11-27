006d720e9836a84abab046e0b142ea3fff18f3ec
Jaewoong Lee <230@fumire.moe>
2020-11-27 12:43:57 +0900

    typo

--- jwlee230/Program/Python/step17.py
+++ jwlee230/Program/Python/step17.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     ancom_data["Bacteria"] = list(map(lambda x: step00.consistency_taxonomy(x), ancom_data["Bacteria"]))
     print(ancom_data)
 
-    data = data[sorted(list(map(lambda x: x[0], list(filter(lambda x: x[1], zip(ancom_data["Bacteria"], ancom_data["Reject"])))))) + ["ShortStage" + "LongStage"]]
+    data = data[sorted(list(map(lambda x: x[0], list(filter(lambda x: x[1], zip(ancom_data["Bacteria"], ancom_data["Reject"])))))) + ["ShortStage", "LongStage"]]
 
     print(data)
     step00.make_pickle(args.output, data)
