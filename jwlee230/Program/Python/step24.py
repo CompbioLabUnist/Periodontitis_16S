7176a5733a6682eefdc41ab179f57462ea44acbe
Jaewoong Lee <230@fumire.moe>
2021-01-08 18:29:36 +0900

    Order the x-label and the y-label

--- jwlee230/Program/Python/step24.py
+++ jwlee230/Program/Python/step24.py
@@ -33,5 +33,8 @@ if __name__ == "__main__":
 
     seaborn.scatterplot(data=data, x=act_column, y=pg_column, hue="LongStage", style="LongStage", ax=ax, legend="full", hue_order=step00.long_stage_order, style_order=step00.long_stage_order, palette=step00.color_stage_order)
 
+    matplotlib.pyplot.xlabel("Actinomyces")
+    matplotlib.pyplot.ylabel("Porphyromonas gingivalis")
+
     fig.savefig(args.output)
     matplotlib.pyplot.close(fig)
