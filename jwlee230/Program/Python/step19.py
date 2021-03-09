4c1824640bfb670a68da39f9ae6db093edbfd1c6
Jaewoong Lee <230@fumire.moe>
2021-03-09 14:26:23 +0900

    Remove circle; Size up the spots

--- jwlee230/Program/Python/step19.py
+++ jwlee230/Program/Python/step19.py
@@ -2,7 +2,6 @@
 step19.py: draw default t-SNE
 """
 import argparse
-import math
 import pandas
 import matplotlib
 import matplotlib.pyplot
@@ -24,19 +23,12 @@ if __name__ == "__main__":
     data["ShortStage"] = list(map(step00.change_ID_into_short_stage, data["ID"]))
     data["LongStage"] = list(map(step00.change_short_into_long, data["ShortStage"]))
 
+    matplotlib.use("Agg")
+    matplotlib.rcParams.update({"font.size": 100, "axes.labelsize": 50, 'axes.titlesize': 100, 'xtick.labelsize': 50, 'ytick.labelsize': 50})
     seaborn.set(context="poster", style="whitegrid")
-    fig, ax = matplotlib.pyplot.subplots(figsize=(24, 24))
+    fig, ax = matplotlib.pyplot.subplots(figsize=(36, 36))
 
-    seaborn.scatterplot(data=data, x="TSNE1", y="TSNE2", hue="LongStage", style="LongStage", ax=ax, legend="full", hue_order=step00.long_stage_order, style_order=step00.long_stage_order, palette=step00.color_stage_order)
-
-    for stage, color in zip(step00.long_stage_order, step00.color_stage_order):
-        selected_data = data.loc[(data["LongStage"] == stage)]
-        x, y = selected_data.mean(axis="index")["TSNE1"], selected_data.mean(axis="index")["TSNE2"]
-
-        r_list = [math.sqrt((x - a) * (x - a) + (y - b) * (y - b)) for a, b in zip(selected_data["TSNE1"], selected_data["TSNE2"])]
-        r = sum(r_list) / len(r_list)
-
-        ax.add_artist(matplotlib.pyplot.Circle((x, y), r, color=color, alpha=0.2))
+    seaborn.scatterplot(data=data, x="TSNE1", y="TSNE2", hue="LongStage", style="LongStage", ax=ax, legend="full", hue_order=step00.long_stage_order, style_order=step00.long_stage_order, palette=step00.color_stage_order, s=1000, edgecolor="none")
 
     fig.savefig(args.output)
     matplotlib.pyplot.close(fig)
