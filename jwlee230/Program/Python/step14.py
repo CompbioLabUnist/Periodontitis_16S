feac8c4bd00469d45b7a2bb0907329c75b6fe7ac
Jaewoong Lee <230@fumire.moe>
2024-01-25 22:13:33 +0900

    Add selected metadata (#11)
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

---
+++ jwlee230/Program/Python/step14.py
@@ -0,0 +1,36 @@
+"""
+step14.py: select metadata
+"""
+import argparse
+import pandas
+import tqdm
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input", help="Input TSV file", type=str)
+    parser.add_argument("output", help="Output TSV file", type=str)
+
+    args = parser.parse_args()
+
+    if not args.input.endswith(".tsv"):
+        raise ValueError("INPUT file must end with .tsv!!")
+    elif not args.output.endswith(".tsv"):
+        raise ValueError("OUTPUT file must end with .tsv!!")
+
+    input_data = pandas.read_csv(args.input, sep="\t", index_col=0, skiprows=[1], keep_default_na=False, na_values={})
+    print(input_data)
+
+    output_data = input_data.loc[(input_data["DB"] == "Korea")]
+    print(output_data)
+
+    with open(args.output, "w") as f:
+        f.write("\t".join(list(output_data.columns)))
+        f.write("\n")
+        f.write("#q2:types\t")
+        f.write("\t".join(["categorical"] * (len(output_data.columns) - 1)))
+        f.write("\n")
+
+        for index, row in tqdm.tqdm(output_data.iterrows(), total=len(output_data)):
+            f.write("\t".join(row))
+            f.write("\n")
