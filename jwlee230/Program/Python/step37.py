362dc6db7eebe4b1f44f6ce7eb4e409a6990915a
Jaewoong Lee <230@fumire.moe>
2024-05-26 20:22:44 +0900

    Upate alpha & beta diversity
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- jwlee230/Program/Python/step37.py
+++ jwlee230/Program/Python/step37.py
@@ -27,6 +27,9 @@ if __name__ == "__main__":
         raise ValueError("OUTPUT must end with .tar!!")
 
     input_data = pandas.read_csv(args.input, sep="\t", index_col=0)
+    input_data = input_data.loc[list(filter(lambda x: not x.startswith("SRR"), list(input_data.index))), :]
+    print(input_data)
+
     input_data["ShortStage"] = list(map(step00.change_ID_into_short_stage, list(input_data.index)))
     input_data["LongStage"] = list(map(step00.change_short_into_long, input_data["ShortStage"]))
     print(input_data)
@@ -38,15 +41,17 @@ if __name__ == "__main__":
     figures = list()
     for alpha in tqdm.tqdm(step00.alphas):
         box_pairs = list()
-        for s1, s2 in itertools.combinations(step00.long_stage_order, r=2):
-            _, p = scipy.stats.ttest_ind(input_data.loc[(input_data["LongStage"] == s1), alpha], input_data.loc[(input_data["LongStage"] == s2), alpha])
+        for s1, s2 in itertools.combinations(step00.long_stage_order[1:], r=2):
+            # _, p = scipy.stats.ttest_ind(input_data.loc[(input_data["LongStage"] == s1), alpha], input_data.loc[(input_data["LongStage"] == s2), alpha])
+            _, p = scipy.stats.mannwhitneyu(input_data.loc[(input_data["LongStage"] == s1), alpha], input_data.loc[(input_data["LongStage"] == s2), alpha])
             if (p < 0.05):
                 box_pairs.append((s1, s2))
 
         fig, ax = matplotlib.pyplot.subplots(figsize=(18, 18))
 
-        seaborn.violinplot(data=input_data, x="LongStage", y=alpha, order=step00.long_stage_order, ax=ax, inner="box", palette=step00.color_stage_dict, cut=1, linewidth=10)
-        statannotations.Annotator.Annotator(ax, box_pairs, data=input_data, x="LongStage", y=alpha, order=step00.long_stage_order).configure(test="t-test_ind", text_format="star", loc="inside", comparisons_correction=None, verbose=0).apply_and_annotate()
+        seaborn.violinplot(data=input_data, x="LongStage", y=alpha, order=step00.long_stage_order[1:], ax=ax, inner="box", palette=step00.color_stage_dict, cut=1, linewidth=10)
+        # statannotations.Annotator.Annotator(ax, box_pairs, data=input_data, x="LongStage", y=alpha, order=step00.long_stage_order[1:]).configure(test="t-test_ind", text_format="star", loc="inside", comparisons_correction=None, verbose=0).apply_and_annotate()
+        statannotations.Annotator.Annotator(ax, box_pairs, data=input_data, x="LongStage", y=alpha, order=step00.long_stage_order[1:]).configure(test="Mann-Whitney", text_format="star", loc="inside", comparisons_correction=None, verbose=True).apply_and_annotate()
 
         matplotlib.pyplot.xlabel("")
         matplotlib.pyplot.ylabel(alpha.replace("_", " "))
--- jwlee230/Program/Python/step38.py
+++ jwlee230/Program/Python/step38.py
@@ -30,9 +30,10 @@ if __name__ == "__main__":
         raise ValueError("OUTPUT must end with .tar!!")
 
     input_data = pandas.read_csv(args.input, sep="\t", index_col=0)
+    input_data = input_data.loc[list(filter(lambda x: not x.startswith("SRR"), list(input_data.index))), list(filter(lambda x: not x.startswith("SRR"), list(input_data.columns)))]
     print(input_data)
 
-    sample_by_stage_dict: typing.Dict[str, typing.List[str]] = {x: list() for x in step00.long_stage_order}
+    sample_by_stage_dict: typing.Dict[str, typing.List[str]] = {x: list() for x in step00.long_stage_order[1:]}
     for sample in tqdm.tqdm(list(input_data.index)):
         sample_by_stage_dict[step00.change_ID_into_long_stage(sample)].append(sample)
 
@@ -41,16 +42,17 @@ if __name__ == "__main__":
     seaborn.set(context="poster", style="whitegrid", rc=step00.matplotlib_parameters)
 
     figures = list()
-    for base_stage in tqdm.tqdm(step00.long_stage_order):
+    for base_stage in tqdm.tqdm(step00.long_stage_order[1:]):
         raw_output = list()
-        for stage in step00.long_stage_order:
+        for stage in step00.long_stage_order[1:]:
             raw_output += [(stage, input_data.loc[a, b]) for a, b in itertools.product(sample_by_stage_dict[base_stage], sample_by_stage_dict[stage])]
 
         output_data = pandas.DataFrame(raw_output, columns=["stage", "distance"])
 
         fig, ax = matplotlib.pyplot.subplots(figsize=(18, 18))
-        seaborn.violinplot(data=output_data, x="stage", y="distance", order=step00.long_stage_order, ax=ax, inner="box", palette=step00.color_stage_dict, cut=1, linewidth=10)
-        statannotations.Annotator.Annotator(ax, list(itertools.combinations(step00.long_stage_order, r=2)), data=output_data, x="stage", y="distance", order=step00.long_stage_order).configure(test="t-test_ind", text_format="star", loc="inside", comparisons_correction=None, verbose=0).apply_and_annotate()
+        seaborn.violinplot(data=output_data, x="stage", y="distance", order=step00.long_stage_order[1:], ax=ax, inner="box", palette=step00.color_stage_dict, cut=1, linewidth=10)
+        # statannotations.Annotator.Annotator(ax, list(itertools.combinations(step00.long_stage_order, r=2)), data=output_data, x="stage", y="distance", order=step00.long_stage_order).configure(test="t-test_ind", text_format="star", loc="inside", comparisons_correction=None, verbose=0).apply_and_annotate()
+        statannotations.Annotator.Annotator(ax, list(itertools.combinations(step00.long_stage_order[1:], r=2)), data=output_data, x="stage", y="distance", order=step00.long_stage_order[1:]).configure(test="Mann-Whitney", text_format="star", loc="inside", comparisons_correction=None, verbose=True).apply_and_annotate()
 
         matplotlib.pyplot.xlabel("")
         matplotlib.pyplot.title(f"Distance to {base_stage}")
